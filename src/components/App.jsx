684fff724a6aae8e538d7feb3f1a7ca752690646
Prateek Patel <43717577+Pr47eek@users.noreply.github.com>
2021-01-26 22:27:15 +0530

    firstCommit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        imglink="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        imglink={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
       />
-      <Card />
+      <Card
+        name={contacts[1].name}
+        imglink={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        imglink={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
